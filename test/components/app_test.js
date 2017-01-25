7b3c97e82567cf6b0372b0f8535bfc99b33fe3ce
Juan Carlos <juancafe2@gmail.com>
2017-01-25 00:08:52 -0400

    Testing

---
+++ test/components/app_test.js
@@ -0,0 +1,11 @@
+import { renderComponent, expect} from '../test_helper';
+import App from '../../src/components/app';
+
+// Use 'describe' to group together similar tests
+describe('App Component')
+
+// Use 'it' to test a single attribute of a target 
+it('Show the correct text')
+
+// Use 'expect' to make an 'assertion' of a target
+expect

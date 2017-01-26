b7db700dd15b310764ab101e5295effe54b18882
Juan Carlos <juancafe2@gmail.com>
2017-01-26 14:28:42 -0400

    Testing components

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,13 @@
+import { renderComponent, expect }  from '../test_helper';
+import  CommentBox from '../../src/components/comment_box';
+
+describe("CommentBox", () => {
+  const component = renderComponent(CommentBox);
+  it('has a text area', () => {
+    expect(component).to.
+  });
+
+  it('has a button', () => {
+
+  });
+})

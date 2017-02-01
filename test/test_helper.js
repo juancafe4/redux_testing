757c1b65dd250e600b704d68685a3136ec3724c0
Juan Carlos <juancafe2@gmail.com>
2017-02-01 00:02:35 -0400

    Render component set up for testing

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,7 +2,11 @@ import jsdom from 'jsdom'
 import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-
+import { expect } from 'chai';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 // Set up testing enviroment to run like a browser in the command line
 // window -> global
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -11,10 +15,15 @@ const $ = _$(global.window); // we want to make responsable of this fake DOM
 
 // Build 'renderComponent' helper that should render a given React class
 function renderComponent(ComponentClass) {
-  const componentInstance = TestUtils.renderDocument(<ComponentClass/>);
+  const componentInstance = TestUtils.renderDocument(
+    <Provider store={createStore(reducers)}>
+      <ComponentClass/>
+    </Provider>
+  );
   return $(ReactDOM.findDOMNode(componentInstance)) // produces HTML
-
 }
 // Build helper for simulating events
 
 // Set up chai-jquery
+
+export { renderComponent, expect };
